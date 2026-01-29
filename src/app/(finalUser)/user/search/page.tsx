466eda127db83c239e6f6442218eab33e13bd804
Saul Zacarias <sezacarias@gmail.com>
2026-01-29 15:49:53 -0400

    feat: banner de requerimiento de identidad

--- src/app/(finalUser)/user/search/page.tsx
+++ src/app/(finalUser)/user/search/page.tsx
@@ -10,6 +10,7 @@ import { Suspense } from "react";
 import SliderSectionSkeleton from "@/src/components/basics/itemsSlider/SliderSectionSkeleton";
 import SearchRecommendedServer from "@/src/components/features/finalUser/search/SearchRecommendedServer";
 import SearchPromoSliderServer from "@/src/components/features/finalUser/search/SearchPromoSliderServer";
+import { AlertCircle } from "lucide-react";
 
 interface PageProps {
   searchParams: Promise<{
@@ -58,12 +59,23 @@ export default async function SearchPage({ searchParams }: PageProps) {
     resolvedSearchParams.type || (types ? types[0] : undefined),
   );
 
+  const isLiquor = types?.includes("liquor");
+
   return (
-    <div className="mx-auto max-w-7xl md:p-4 md:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl md:p-4 md:px-6 lg:px-8 text-black">
       {/* Mobile-only Header Overrides */}
       <CustomSearchHeader title={title} icon={icon} />
 
-      <div className="pt-10 px-4 md:px-0">
+      {isLiquor && (
+        <div className="md:hidden flex items-center justify-center gap-2 w-full h-12 bg-[#FFCF58]/[0.13] px-6 mt-4">
+          <AlertCircle className="w-[18px] h-[18px] text-[#CF4518]" />
+          <span className="text-[#E94E1B] font-[Open Sans] font-semibold text-[13px] leading-[17px] text-center">
+            Mostrar un ID válido que confirme 18+
+          </span>
+        </div>
+      )}
+
+      <div className="pt-6 px-4 md:px-0">
         {/* Search Bar Mobile */}
         <MobileSearchBar placeholder="Busca en El Nacional" />
 
