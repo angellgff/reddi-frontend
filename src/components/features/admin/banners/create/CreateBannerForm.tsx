6aefef0236f5744c2f57d689d2a6a5c467511e91
Saul Zacarias <sezacarias@gmail.com>
2026-01-29 12:46:47 -0400

    feat: add improvements

--- src/components/features/admin/banners/create/CreateBannerForm.tsx
+++ src/components/features/admin/banners/create/CreateBannerForm.tsx
@@ -85,8 +85,8 @@ export default function CreateBannerForm({
 
         // 1. Upload Image
         if (!imageFile) {
-           setGlobalError("La imagen es obligatoria.");
-           return;
+          setGlobalError("La imagen es obligatoria.");
+          return;
         }
         const imageUrl = await uploadFile(imageFile, "banners", "images");
 
